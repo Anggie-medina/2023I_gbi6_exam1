f7f383623a833441dc2f5f249712ac994026f536
Anggie-medina <130715331+Anggie-medina@users.noreply.github.com>
2023-05-25 01:57:05 -0500

    primera

---
+++ exam1/denvint.sh
@@ -0,0 +1,20 @@
+# Segunda Pregunta
+directorio="/c/Users/Mika/Documents/GitHub/2023I_gbi6_exam1/exam1/data/denvint"
+
+# Archivos .csv
+archivos=$(ls "$directorio"/*.csv)
+
+# Conteo del numero de filas y columnas de los archivos
+for file in $archivos; do
+    # Nombre del archivo
+    filename=$(basename "$file")
+
+    # Contar filas
+    rows=$(cat "$file" | wc -l)
+
+    # Contar columnas
+    columns=$(head -n 1 "$file" | tr ',' '\n' | wc -l)
+
+    # Imprimir resultados
+    echo "Archivo: $filename"
+    echo "Número de filas: $rows"
