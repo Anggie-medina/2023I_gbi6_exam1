b28bccaa5cc5b1278ad181f698e96d67906f9062
Anggie-medina <130715331+Anggie-medina@users.noreply.github.com>
2023-05-25 21:34:08 -0500

    ultima pregunta
    Rani modificaciones

--- exam1/metagen.sh
+++ exam1/metagen.sh
@@ -3,12 +3,5 @@ cd data/metagen
 
 grep "s__" infants_metagenome.txt | wc -l
 
-echo "TATA" > gata.txt
-grep -o "\w*TATA\w*" mygenomemap.sam >> gata.txt
 
-echo "GAGA" > gata.txt
-grep -o "\w*GAGA\w*" mygenomemap.sam >> gata.txt
-
-echo "GATA" > gata.txt
-grep -o "\w*GATA\w*" mygenomemap.sam >> gata.txt
-
+grep -E "TATA|GAGA|GATA" mygenomemap.sam >> /c/Users/Mika/Documents/GitHub/2023I_gbi6_exam1/exam1/gata.txt
