ff743bdd1f78009a46fb83fc650f2b44b16bda38
Anggie-medina <130715331+Anggie-medina@users.noreply.github.com>
2023-05-25 22:26:58 -0500

    chao

--- exam1/metagen.sh
+++
@@ -1,7 +0,0 @@
-#Pregunta 5
-cd data/metagen
-
-grep "s__" infants_metagenome.txt | wc -l
-
-
-grep -E "TATA|GAGA|GATA" mygenomemap.sam >> /c/Users/Mika/Documents/GitHub/2023I_gbi6_exam1/exam1/gata.txt
--- exam1/singlecell.sh
+++
@@ -1,7 +0,0 @@
-#Primera pregunta
-
-singlecell="/c/Users/Mika/Documents/GitHub/2023I_gbi6_exam1/exam1/data"
-
-ls -R "$singlecell" | grep -o -E '\.[a-zA-Z0-9]+$' | sort | uniq -c > extensiones.txt
-
- echo "Conteo de extensiones de archivos completado y guardado en extensiones.txt"
